89415b027125f3062efda9bee75902c4e534cfb8
rajanga <ganeshbabu.rajan@rabobank.nl>
2020-12-25 09:19:02 +0100

    integration test case has been added for the controller flux endpoint

--- src/test/java/com/learnreactive/reactivedemo/controller/FluxControllerTest.java
+++ src/test/java/com/learnreactive/reactivedemo/controller/FluxControllerTest.java
@@ -1,7 +1,66 @@
 package com.learnreactive.reactivedemo.controller;
 
 import junit.framework.TestCase;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.reactive.server.EntityExchangeResult;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import reactor.core.publisher.Flux;
+import reactor.test.StepVerifier;
 
+import java.util.Arrays;
+import java.util.List;
+
+@RunWith(SpringRunner.class)
+@WebFluxTest
 public class FluxControllerTest extends TestCase {
 
+    @Autowired
+    WebTestClient testClient;
+
+    @Test
+    public void Test_Approach_1_ReturnResult() {
+
+        Flux<Integer> responseBody = testClient.get().uri("/flux")
+                .accept(MediaType.APPLICATION_JSON_UTF8)
+                .exchange()
+                .expectStatus().isOk()
+                .returnResult(Integer.class)
+                .getResponseBody();
+
+        StepVerifier.create(responseBody.log())
+                .expectSubscription()
+                .expectNext(1, 2, 3, 4, 5)
+                .verifyComplete();
+    }
+
+    @Test
+    public void Test_Approach_2_VerifyBody() {
+        testClient.get().uri("/flux")
+                .accept(MediaType.APPLICATION_JSON_UTF8)
+                .exchange()
+                .expectStatus().isOk()
+                .expectHeader().contentType(MediaType.APPLICATION_JSON_UTF8)
+                .expectBodyList(Integer.class)
+                .hasSize(5);
+    }
+
+    @Test
+    public void Test_Approach_3_EntityExchangeResult() {
+        EntityExchangeResult<List<Integer>> listEntityExchangeResult = testClient.get().uri("/flux")
+                .accept(MediaType.APPLICATION_JSON_UTF8)
+                .exchange()
+                .expectStatus().isOk()
+                .expectHeader().contentType(MediaType.APPLICATION_JSON_UTF8)
+                .expectBodyList(Integer.class)
+                .returnResult();
+
+        List<Integer> expectedListOfIntegers = Arrays.asList(1, 2, 3, 4, 5);
+
+        assertEquals(expectedListOfIntegers, listEntityExchangeResult.getResponseBody());
+    }
 }
